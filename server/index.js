bf100e6d55cfcefcb238be55719059e0e7110135
hollygrosario <hollyrosario@outlook.com>
2022-05-10 20:12:51 -0400

    Install passport and configure oauth

--- server/index.js
+++ server/index.js
@@ -1,12 +1,44 @@
 // import express library
 const express = require('express')
+// import passport
+const passport = require('passport')
+// import passport strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+// require client id and secret keys
+const keys = require('./config/keys')
+
 // generate a new running express app
 const app = express()
 
-// create a route handler and associate it with a given route
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+// console.developers.google.com
 
+// create a new instance of the google strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken:', accessToken)
+      console.log('refreshToken:', refreshToken)
+      console.log('profile:', profile)
+    }
+  )
+)
+
+// route handler to get directed into the passport authentication flow
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
+
+// second route handler for passport to exchange a code to get the profile info from google
+app.get('/auth/google/callback', passport.authenticate('google'))
+
+const PORT = process.env.PORT || 3000
 // express is telling node to listen for traffic on this  port
-app.listen(3000)
+app.listen(PORT)
