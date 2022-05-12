e0a6046dc0d3c5efac5c5616fcd41af0d8a8fd8e
hollygrosario <hollyrosario@outlook.com>
2022-05-11 23:09:37 -0400

    Complete route handlers for auth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,16 +2,21 @@
 const passport = require('passport')
 
 // wrap the routes in module.export to pass in the original instance of the express app
-module.exports = (app) => {
-// console.developers.google.com
-// route handler to get directed into the passport authentication flow
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-)
+module.exports = app => {
+  // console.developers.google.com
+  // route handler to get directed into the passport authentication flow
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  )
 
-// second route handler for passport to exchange a code to get the profile info from google
-app.get('/auth/google/callback', passport.authenticate('google'))
+  // second route handler for passport to exchange a code to get the profile info from google
+  app.get('/auth/google/callback', passport.authenticate('google'))
+
+  // test route for auth
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user)
+  })
 }
