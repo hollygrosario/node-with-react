171457c215c4594e1f18882b1b59dc543519263f
hollygrosario <hollyrosario@outlook.com>
2022-05-11 22:56:30 -0400

    Passport callback/encode user/deserialize user/enable cookies

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@
 const express = require('express')
 // import mongoose
 const mongoose = require('mongoose')
+// require helper library for cookies
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys')
 // require users model
 require('./models/User')
@@ -12,6 +15,18 @@ mongoose.connect(keys.mongoURI)
 
 // generate a new running express app
 const app = express()
+
+// make use of cookies in the app
+// 30 days before expiration
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+// tell passport to make use of the cookies for auth
+app.use(passport.initialize())
+app.use(passport.session())
 
 // require authRoutes file that returns a function and pass the app instance to the function
 require('./routes/authRoutes')(app)
--- server/services/passport.js
+++ server/services/passport.js
@@ -11,6 +11,24 @@ const keys = require('../config/keys')
 
 const User = mongoose.model('users')
 
+// user.id shortcute to the mongo _id
+// stuff that id into a cookie
+// turn the user into an id
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+// take the id stuffed in the cookie and turn it back into a user models
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id)
+    console.log('user:', user)
+    done(null, user)
+  } catch (e) {
+    console.log('e:', e)
+  }
+})
+
 // create a new instance of the google strategy
 // configuration of google strategy
 passport.use(
@@ -20,9 +38,20 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    (accessToken, refreshToken, profile, done) => {
-      // create a new instance of a user and save it to the db
-      new User({ googleId: profile.id }).save()
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const existingUser = await User.findOne({ googleId: profile.id })
+        if (existingUser) {
+          // we have a user already
+          console.log('existingUser:', existingUser)
+          return done(null, existingUser)
+        }
+        // query the db and find one user with the matching  google id
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user)
+      } catch (e) {
+        console.log('e:', e)
+      }
     }
   )
 )
