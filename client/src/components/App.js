7bc1cca04ac2d74421b4b53674885beac969700b
hollygrosario <hollyrosario@outlook.com>
2022-05-16 09:34:08 -0400

    Router set-up and CSS configuration

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Header from './Header'
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>Survey New</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
-  return <div>Hi There!</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/surveys' element={<Dashboard />} />
+          <Route path='/surveys/new' element={<SurveyNew />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
