97f6b8cf85884ce8936be1c6793c21fb10cddc14
hollygrosario <hollyrosario@outlook.com>
2022-05-30 07:46:20 -0400

    Landing component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,15 +4,16 @@ import { connect } from 'react-redux'
 import * as actions from '../actions'
 
 import Header from './Header'
+import Landing from './Landing'
 
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>Survey New</h2>
-const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
-  componentDidMount(){
+  componentDidMount () {
     this.props.fetchUser()
   }
+
   render () {
     return (
       <div className='container'>
@@ -29,4 +30,4 @@ class App extends Component {
   }
 }
 
-export default connect (null, actions) (App)
+export default connect(null, actions)(App)
