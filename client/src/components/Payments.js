7c9dd80e6a42011cba2635f412ed5638e25303de
hollygrosario <hollyrosario@outlook.com>
2022-05-31 19:21:50 -0400

    Wire up payments to app

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
 class Payments extends Component {
   render () {
@@ -8,7 +10,7 @@ class Payments extends Component {
         name='node-with-react'
         description='$5 for 5 email credits'
         amount={500}
-        token={token => console.log('token:', token)}
+        token={token => this.props.handleToken(token)}
         stripeKey='pk_test_51L58x8AtSg8Cfs26xnm5Jo8KPsA3ZrcEYNECUehfvFrDboRDCt2EgejFcanfHXMdSPLWlq5dXuv0iXuSkuXr3KUU004xKDWtcT'
       >
         <button className='btn'>Add Credits</button>
@@ -17,4 +19,4 @@ class Payments extends Component {
   }
 }
 
-export default Payments
+export default connect(null, actions)(Payments)
