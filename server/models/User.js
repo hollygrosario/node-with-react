e75fd3ec4d2120d0236fb056e869e0442d992b5b
hollygrosario <hollyrosario@outlook.com>
2022-06-05 15:44:52 -0400

    Route-specific middleware

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,8 @@ const { Schema } = mongoose
 
 // create the schema
 const userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  credits: { type: Number, default: 0 }
 })
 
 mongoose.model('users', userSchema)
