9223c8e04762504142e35fd0b29edd5b7fc2c06c
hollygrosario <hollyrosario@outlook.com>
2022-06-21 15:14:21 -0400

    Finish the survey route handler

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 const Survey = mongoose.model('surveys')
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body
 
     const survey = new Survey({
@@ -20,6 +20,19 @@ module.exports = app => {
     })
     // great place to send an email
     const mailer = new Mailer(survey, surveyTemplate(survey))
-    mailer.send()
+
+    try {
+      await mailer.send()
+      // save the survey
+      await survey.save()
+      // minus a credit
+      req.user.credits -= 1
+      // save the user from the req
+      const user = await req.user.save()
+      // send back the res with the updated user
+      res.send(user)
+    } catch (err) {
+      res.status(422).send(err)
+    }
   })
 }
